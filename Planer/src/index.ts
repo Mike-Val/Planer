dabcd6ebfe334f33212a1ed4dbbf892f9c612305
DryValley <michelangelo.valsecchi@gmail.com>
2024-01-22 13:23:47 +0000

    Planer - Added tests and fixed bug: `PCA.isCovarianceMatrix` was set to true while it should have been false

--- Planer/src/index.ts
+++ Planer/src/index.ts
@@ -8,19 +8,29 @@ export interface Options {
     plane?: Plane;
 }
 
+export type PlaneResult = {
+    plane: Plane;
+    center?: Vector3;
+}
 
-export function findPlane(points: number[][] | Vector3[], options: Options = {}): Plane | null {
-    if (points.length < 3) return null;
+
+export function findPlane(points: number[][] | Vector3[], options: Options = {}): PlaneResult | null {
+    if (!points || points.length < 3) return null;
     
-    let _points = points as number[][];
-
+    let _points;
     if (options.unsafe 
         ? points[0] instanceof Vector3 
         : points.every(point => point instanceof Vector3)
     ) _points = (points as Vector3[]).map(point => point.toArray());
+    else _points = options.unsafe ? points as number[][] : (points as number[][]).map(point => [...point]);
 
     const opt = { ...options.PCAOptions };
-    opt.isCovarianceMatrix = true;
+    opt.isCovarianceMatrix = false;
+
+    const pca = new PCA(_points, opt);
+    const eigenValues = pca.getEigenvalues();
+    const smallestEigenValue = eigenValues.indexOf(Math.min(...eigenValues));
+    const normal = new Vector3(...pca.getEigenvectors().getColumn(smallestEigenValue)).normalize();
 
     const center = new Vector3(..._points.reduce((acc, point) => {
         acc[0] += point[0];
@@ -29,13 +39,8 @@ export function findPlane(points: number[][] | Vector3[], options: Options = {})
         return acc;
     }, [0, 0, 0]).map(value => value / _points.length));
 
-    const pca = new PCA(_points, opt);
-    const eigenValues = pca.getEigenvalues();
-    const smallestEigenValue = eigenValues.indexOf(Math.min(...eigenValues));
-    const normal = new Vector3(...pca.getEigenvectors().getColumn(smallestEigenValue));
-
     const plane = options.plane ?? new Plane();
     plane.setFromNormalAndCoplanarPoint(normal, center);
 
-    return plane;
+    return { plane, center };
 }
